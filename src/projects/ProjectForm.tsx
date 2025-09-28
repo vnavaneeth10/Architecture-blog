9d565d3f5ae0e66dbffd58a6138a688e5a63d5ee
Navaneeth V <navaneethnair69@gmail.com>
2025-09-28 22:52:33 +0530

    refactor: clean up commented code and improve inline documentation in ProjectForm, ProjectCard, and ProjectList components

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -3,28 +3,52 @@ import { Project } from "./Project"
 import { motion } from "framer-motion";
 
 
+
 interface ProjectFormProps {
+    // project is optional because when adding a new project there is no initial project
+    // but when editing there is an initial project to edit
+    // when no project is being edited the value is undefined
+    // when adding a new project the value is {}
+    // when editing a project the value is that project object
     project?: Project;
+
     onSave: (project: Project) => void
+    // function to save the project
     onCancel: () => void
+    // function to cancel the editing
 }
-const ProjectForm = ({ project: initialProject, onSave, onCancel }: ProjectFormProps) => {
-    //rename it initialProject so that we can name our state variable project
-    // thi is the form component
+const ProjectForm = ({ 
+    project: initialProject, 
+    onSave, 
+    onCancel 
+}: ProjectFormProps) => {
+    // if there is no initial project (when adding a new project) set the initial project to an empty object
     const [project, setProject] = useState(initialProject)
+    // state to keep track of the project being edited or added
+    const[errors,setErrors] = useState({
+        name:'',
+        description:'',
+        budget:''
+    }) // state to keep track of the errors in the form fields
 
     const handleSubmit = (e: SyntheticEvent) => {
+        //function to handle the form submission
         e.preventDefault();
         // onSave(new Project({ name: 'Updated Project' }))
         onSave(project);
     }
 
     const handleChange = (event: any) => {
-
+        // console.log(event.target.value);
         const { type, name, value, checked } = event.target;
         // if input type is checkbox use checked
         // otherwise it's type is text, number etc. so use value
         let updatedValue = type === 'checkbox' ? checked : value;
+        // getting the updated value from the input field
+
+        //  getting the name of the input field
+        // name attribute of the input field is used to identify the input field
+        // this is used to update the corresponding property of the project object
 
 
         //if input type is number convert the updatedValue string to a number
@@ -37,6 +61,7 @@ const ProjectForm = ({ project: initialProject, onSave, onCancel }: ProjectFormP
         }
 
         let updatedProject: Project;
+        // variable to hold the updated project object
 
         // need to do functional update b/c
         // the new project state is based on the previous project state
@@ -44,53 +69,80 @@ const ProjectForm = ({ project: initialProject, onSave, onCancel }: ProjectFormP
         // the spread operator (...) is used to  spread the previous project properties and the new change
 
         setProject((p) => {
+            console.log("p", p)
+            console.log(" b4 updatedProject", updatedProject)
             updatedProject = new Project({ ...p, ...change })
+            console.log("after updatedProject", updatedProject)
             return updatedProject
         })
+        // setting the project state to the updated project object
+
 
     }
 
 
     return (
         <>
-            <form className="input-group vertical" onSubmit={handleSubmit}>
+            <form
+                className="input-group vertical"
+                onSubmit={handleSubmit}
+            >
                 {/* label of the name */}
-                <label htmlFor="name">Project Name</label>
+
+                <label
+                    htmlFor="name"
+                >Project Name
+                </label>
+
                 {/* input type name */}
-                <input 
-                type="text" 
-                name="name" 
-                placeholder="enter name" 
-                value={project.name}
-                onChange={handleChange}
+
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="enter name"
+                    value={project.name}
+                    onChange={handleChange}
                 />
+
                 {/* label of the description */}
                 <label htmlFor="description">Project Description</label>
                 {/* description text are content */}
-                <textarea name="description" placeholder="enter description" value={project.description} onChange={handleChange}/>
+                <textarea name="description" placeholder="enter description" value={project.description} onChange={handleChange} />
                 {/* label of the budget */}
                 <label htmlFor="budget">Project Budget</label>
                 {/* budget input area */}
-                <input type="number" name="budget" placeholder="enter budget" value={project.budget}  onChange={handleChange}/>
+                <input type="number" name="budget" placeholder="enter budget" value={project.budget} onChange={handleChange} />
                 {/* label of the checkbox */}
                 <label htmlFor="isActive">Active?</label>
                 {/* checkbox input element */}
-                <input type="checkbox" name="isActive" checked={project.isActive} onChange={handleChange}/>
+                <input type="checkbox" name="isActive" checked={project.isActive} onChange={handleChange} />
 
                 <div className="input-group">
                     {/* Button to save the filled form data */}
-                    <motion.button className="primary bordered medium" whileHover={{
-                        opacity: 1,
-                        scale: 1,
-                        boxShadow: "0px 2px 2px #222",
-                    }}>Save</motion.button>
+                    <motion.button
+                        className="primary bordered medium"
+                        whileHover={{
+                            opacity: 1,
+                            scale: 1,
+                            boxShadow: "0px 2px 2px #222",
+                        }}>
+                        Save
+                    </motion.button>
+
                     <span></span>
+
                     {/* Button to cancel*/}
-                    <motion.button type="button" className="bordered medium" onClick={onCancel} whileHover={{
-                        opacity: 1,
-                        scale: 1,
-                        boxShadow: "0px 2px 2px #222",
-                    }}>cancel</motion.button>
+                    <motion.button
+                        type="button"
+                        className="bordered medium"
+                        onClick={onCancel}
+                        whileHover={{
+                            opacity: 1,
+                            scale: 1,
+                            boxShadow: "0px 2px 2px #222",
+                        }}>Cancel
+                    </motion.button>
+
                 </div>
             </form>
         </>
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -5,46 +5,77 @@ import ProjectForm from './ProjectForm';
 
 //ProjectListProps used to mention the type of the props of passed project data
 interface ProjectListProps {
-    projects: Project[];  //since it is in array format hence mentioned in array format
-    onSave: (project: Project) => void
+    projects: Project[];  //array of projects
+    onSave: (project: Project) => void //function to save a project
 }
 
 const ProjectList = ({ projects, onSave }: ProjectListProps) => {
     // Props are passed here  and the interface is mentioned with destructuring
 
-    const [projectBeingEdited, setProjectBeingEdited] = useState()
+    const [projectBeingEdited, setProjectBeingEdited] = useState<Project | undefined>()
+    // state to keep track of which project is being edited
+    // initially no project is being edited so undefined
+    // when a project is being edited the state is set to that project object
+    // when no project is being edited the state is set to undefined
 
     const cancelEditing = () => {
+        //function to cancel the editing of the project
         setProjectBeingEdited({})
+
     }
+
     //this is the  function in parent component used to change the edit of the card component
     const handleEdit = (project: Project) => {
         // console.log(project)
-        setProjectBeingEdited(project);
+        setProjectBeingEdited(project);// setting the project being edited to the project passed from the child component
     }
 
     return (
-        <div className="row">
-            
+
+        <div
+            className="row">
+
             {/* the passed mock data is mapped or iterated here and passed to the Project Card component as props*/}
 
-            {projects.map((project) => (
+            {
+                projects.map((project) => (
+                    //Iterates over all projects. Each project is passed to a ProjectCard component.
+                    // Each ProjectCard component is given a unique key based on the project's id.
+                    // The onEdit function is passed to each ProjectCard to handle editing actions.
+                    // If a project is being edited, a ProjectForm component is rendered instead of the ProjectCard.
+                    // The ProjectForm component receives the project data and functions for saving and canceling edits.
+                    <div
+                        key={project.id}
+                        className="cols-sm"
+                    >
 
-                <div key={project.id} className="cols-sm">
+                        { // Ternary operator to check if the current project is being edited
+                            project === projectBeingEdited ? 
+                                (
+                                    // if the project is being edited show the form component
+                                    // and pass the project being edited as props
 
-                    {
-                        project === projectBeingEdited ?
-                            (<ProjectForm onCancel={cancelEditing} onSave={onSave} project={project}/>)
-                            :
-                            (<ProjectCard project={project} onEdit={handleEdit} />)
-                    }
+                                    <ProjectForm
+                                        onCancel={cancelEditing}
+                                        onSave={onSave}
+                                        project={project}
+                                    />
 
+                                )
 
-                    {/* function and iterated data is passed as props */}
+                                :
 
-                    {/* this is the form component which is a child one */}
-                </div>
-            ))}
+                                ( 
+                                    // else show the project card component
+                                    <ProjectCard
+                                        project={project}
+                                        onEdit={handleEdit}
+                                    />
+                                )
+                        }
+
+                    </div>
+                ))}
         </div>
 
 
